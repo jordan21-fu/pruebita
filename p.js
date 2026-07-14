904a74f2e8afda4b88410211fcc787d7d13c2d3d
Jordan222 <jordan2020outlook@gmail.com>
2026-07-13 22:21:32 -0300

    Add files via upload

--- p.js
+++ p.js
@@ -1,4 +1,29 @@
-function info(){
-    objeto= 
-    return console.log("gola");
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+function abrir(){
+    menu.classList.toggle("active");
 }
+
+const elementos = document.querySelectorAll(
+    ".abajo, .arriba, .izquierda, .derecha"
+);
+
+const observer = new IntersectionObserver((entradas) => {
+
+    entradas.forEach((entrada) => {
+
+        if (entrada.isIntersecting) {
+            entrada.target.classList.add("visible");
+        } else {
+            entrada.target.classList.remove("visible");
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+elementos.forEach((elemento) => {
+    observer.observe(elemento);
+});
